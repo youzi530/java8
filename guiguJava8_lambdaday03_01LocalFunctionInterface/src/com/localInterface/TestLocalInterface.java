de83bb071d4b406527e784b998db12016c41812e
lingqiao <2676272310@qq.com>
2021-10-16 21:44:34 +0800

    [mac]feat:add the local interface

--- guiguJava8_lambdaday03_01LocalFunctionInterface/src/com/localInterface/TestLocalInterface.java
+++ guiguJava8_lambdaday03_01LocalFunctionInterface/src/com/localInterface/TestLocalInterface.java
@@ -3,6 +3,7 @@ package com.localInterface;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -21,17 +22,17 @@ public class TestLocalInterface {
 
     }
 
-    //Consumer
+    //Consumer  有去无回
     public static void happy(double money, Consumer<Double> consumer) {
         consumer.accept(money);
     }
 
     @Test
-    public void test1() {
+    public void testConsumer() {
         happy(1000, (money) -> System.out.println("买酒了"));
     }
 
-    //Supplier  产生指定个数的整数，并放入集合中
+    //Supplier  产生指定个数的整数，并放入集合中   产生一些对象的
     public static List<Integer> getNumber(int num, Supplier<Integer> supplier) {
         ArrayList<Integer> result = new ArrayList<>();
 
@@ -43,7 +44,7 @@ public class TestLocalInterface {
     }
 
     @Test
-    public void test2() {
+    public void testSupplier() {
         List<Integer> number = getNumber(10, () -> (int) (Math.random() * 100));
         for (Integer i : number) {
             System.out.println(i);
@@ -56,12 +57,12 @@ public class TestLocalInterface {
     }
 
     @Test
-    public void test3() {
-        String s = strHandler("\t\t\t\t4214kfewbfi服务器功能及其嗯  rwqke", (x) -> x.trim());
+    public void testFunction() {
+        String s = strHandler("\t\t\t\t4214kfewbfi服务器功能及其嗯  rwqke  ", (x) -> x.trim());
         System.out.println(s);
     }
 
-    //Predicate<T>断言型接口:
+    //Predicate<T>断言型接口:将指定的字符串（满足条件的）添加到集合中
     public static List<String> filterStr(List<String> list, Predicate<String> predicate){
         List<String> result = new ArrayList<>();
         for(String str : list){
@@ -70,5 +71,14 @@ public class TestLocalInterface {
             }
         }
         return result;
+    }
+
+    @Test
+    public void testPredicate() {
+        List<String> list = Arrays.asList("hello", "lingqiao", "lambda", "ww");
+        List<String> reslut = filterStr(list, (str) -> str.length() > 3);
+        for (String s:reslut) {
+            System.out.println(s);
+        }
     }
 }
