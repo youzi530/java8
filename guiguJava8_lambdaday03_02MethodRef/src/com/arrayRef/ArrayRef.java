33fa022835008ad456eff5b4d33cf9e364eb438e
lingqiao <2676272310@qq.com>
2021-10-18 14:37:39 +0800

    [mac]feat:数组引用

--- guiguJava8_lambdaday03_02MethodRef/src/com/arrayRef/ArrayRef.java
+++ guiguJava8_lambdaday03_02MethodRef/src/com/arrayRef/ArrayRef.java
@@ -1,5 +1,25 @@
 package com.arrayRef;
 
+import org.junit.Test;
+
+import java.util.function.Function;
+
 public class ArrayRef {
-    
+    /**
+     * 数组引用：
+     *
+     * Type::new
+     */
+
+    @Test
+    public void test(){
+        Function<Integer,String[]> function = (x)->new String[x];
+        String[] apply = function.apply(10);
+        System.out.println(apply.length);
+
+        Function<Integer,String[]> function1 = String[]::new;
+        String[] apply1 = function1.apply(20);
+        System.out.println(apply1.length);
+    }
+
 }
