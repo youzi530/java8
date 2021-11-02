e79e96b9c7dc613378c9b2d48bd61ac82d85de57
lingqiao <2676272310@qq.com>
2021-11-02 17:17:55 +0800

    [lingqiao]feat: practice use

--- guiguJava8_lambdaday04_02StreamOptions_FilterAndSplit/src/stream/StreamCollect.java
+++ guiguJava8_lambdaday04_02StreamOptions_FilterAndSplit/src/stream/StreamCollect.java
@@ -3,8 +3,10 @@ package stream;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * 收集                     方 法 描 述
@@ -12,47 +14,47 @@ import java.util.stream.IntStream;
  * <p>
  * Collector 接口中方法的实现决定了如何对流执行收集操作(如收集到 List、Set、Map)。
  * 但是 Collectors 实用类提供了很多静态方法，可以方便地创建常见收集器实例，具体方法与实例如下表：
- *
+ * <p>
  * 方法                       返回类型               作用
  * toList                     List<T>              把流中元素收集到List
  * List<Employee> emps= list.stream().collect(Collectors.toList());
- *
+ * <p>
  * toSet                      Set<T>               把流中元素收集到Set
  * Set<Employee> emps= list.stream().collect(Collectors.toSet());
- *
+ * <p>
  * toCollection               Collection<T>        把流中元素收集到创建的集合
  * Collection<Employee>emps=list.stream().collect(Collectors.toCollection(ArrayList::new));
- *
+ * <p>
  * counting                   Long                 计算流中元素的个数
  * long count = list.stream().collect(Collectors.counting());
- *
+ * <p>
  * summingInt                 Integer              对流中元素的整数属性求和
  * inttotal=list.stream().collect(Collectors.summingInt(Employee::getSalary));
- *
+ * <p>
  * averagingInt               Double               计算流中元素Integer属性的平均值
  * doubleavg= list.stream().collect(Collectors.averagingInt(Employee::getSalary));
- *
+ * <p>
  * summarizingInt            IntSummaryStatistics   收集流中Integer属性的统计值。如：平均值
  * IntSummaryStatisticsiss= list.stream().collect(Collectors.summarizingInt(Employee::getSalary));
- *
+ * <p>
  * joining                   String                 连接流中每个字符串
  * String str= list.stream().map(Employee::getName).collect(Collectors.joining());
- *
+ * <p>
  * maxBy                     Optional<T>            根据比较器选择最大值
  * Optional<Emp>max= list.stream().collect(Collectors.maxBy(comparingInt(Employee::getSalary)));
- *
+ * <p>
  * minBy                     Optional<T>            根据比较器选择最小值
  * Optional<Emp> min = list.stream().collect(Collectors.minBy(comparingInt(Employee::getSalary)));
- *
+ * <p>
  * reducing                  归约产生的类型            从一个作为累加器的初始值开始，利用BinaryOperator与流中元素逐个结合，从而归约成单个值
  * inttotal=list.stream().collect(Collectors.reducing(0, Employee::getSalar, Integer::sum));
- *
+ * <p>
  * collectingAndThen         转换函数返回的类型         包裹另一个收集器，对其结果转换函数
  * inthow= list.stream().collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
- *
+ * <p>
  * groupingBy                Map<K, List<T>>         根据某属性值对流分组，属性为K，结果为V
  * Map<Emp.Status, List<Emp>> map= list.stream().collect(Collectors.groupingBy(Employee::getStatus));
- *
+ * <p>
  * partitioningBy            Map<Boolean, List<T>>   根据true或false进行分区
  * Map<Boolean,List<Emp>>vd= list.stream().collect(Collectors.partitioningBy(Employee::getManage));
  */
@@ -188,7 +190,7 @@ public class StreamCollect {
     @Test
     public void test9() {
         // 将字符串换成大写并用逗号链接起来
-        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
         String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
         System.out.println(G7Countries);
     }
@@ -222,8 +224,8 @@ public class StreamCollect {
                 .forEach(System.out::println);
 
         //示例2:mapToInt()在执行具有其长度的映射字符串的操作后返回IntStream。
-        List<String> list1 = Arrays.asList("Geeks", "for", "gfg","GeeksforGeeks", "GeeksQuiz");
-        list1.stream().mapToInt(str->str.length()).forEach(System.out::println);
+        List<String> list1 = Arrays.asList("Geeks", "for", "gfg", "GeeksforGeeks", "GeeksQuiz");
+        list1.stream().mapToInt(str -> str.length()).forEach(System.out::println);
     }
 
     //peek
@@ -238,5 +240,50 @@ public class StreamCollect {
                 })
                 .collect(Collectors.toList());
         System.out.println(list2);
+    }
+
+    @Test
+    public void test13() {
+        //两个新的方法可在字符串类上使用: join和chars。第一个方法使用指定的分隔符，将任何数量的字符串连接为一个字符串。
+        String str = String.join(":", "221", "test");
+        System.out.println(str);
+    }
+
+    @Test
+    public void test14() {
+        String str = String.join(":", "221", "test");
+
+        //第二个方法chars从字符串所有字符创建数据流，所以你可以在这些字符上使用流式操作。
+        String collect = str.chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf(c))
+                .sorted()
+                .collect(Collectors.joining());
+        System.out.println(collect);
+
+        //解析：
+        IntStream chars = str.chars();
+        Stream<String> stringStream = chars.mapToObj(c -> String.valueOf(c));
+        String collect1 = stringStream.collect(Collectors.joining());
+    }
+
+    @Test
+    public void test15() {
+        //不仅仅是字符串，正则表达式模式串也能受益于数据流。我们可以分割任何模式串，并创建数据流来处理它们，而不是将字符串分割为单个字符的数据流，像下面这样:
+        String bar = Pattern.compile(":")
+                .splitAsStream("foobar:foo:bar")
+                .filter(s -> s.contains("bar"))
+                .sorted()
+                .collect(Collectors.joining(":"));
+        System.out.println(bar);
+    }
+
+    @Test
+    public void test16() {
+        Pattern pattern = Pattern.compile(".*@gmail\\.com");
+        long count = Stream.of("bob@gmail.com", "alice@hotmail.com")
+                .filter(pattern.asPredicate())
+                .count();
+        System.out.println(count);
     }
 }
