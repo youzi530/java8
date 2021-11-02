7321af6111a0bdc499fe72e30c9b9e3243697266
lingqiao <2676272310@qq.com>
2021-11-02 10:57:03 +0800

    [lingqiao]feat:summaryStatistics & mapToInt

--- guiguJava8_lambdaday04_02StreamOptions_FilterAndSplit/src/stream/StreamCollect.java
+++ guiguJava8_lambdaday04_02StreamOptions_FilterAndSplit/src/stream/StreamCollect.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * 收集                     方 法 描 述
@@ -11,33 +12,47 @@ import java.util.stream.Collectors;
  * <p>
  * Collector 接口中方法的实现决定了如何对流执行收集操作(如收集到 List、Set、Map)。
  * 但是 Collectors 实用类提供了很多静态方法，可以方便地创建常见收集器实例，具体方法与实例如下表：
+ *
  * 方法                       返回类型               作用
  * toList                     List<T>              把流中元素收集到List
  * List<Employee> emps= list.stream().collect(Collectors.toList());
+ *
  * toSet                      Set<T>               把流中元素收集到Set
  * Set<Employee> emps= list.stream().collect(Collectors.toSet());
+ *
  * toCollection               Collection<T>        把流中元素收集到创建的集合
  * Collection<Employee>emps=list.stream().collect(Collectors.toCollection(ArrayList::new));
+ *
  * counting                   Long                 计算流中元素的个数
  * long count = list.stream().collect(Collectors.counting());
+ *
  * summingInt                 Integer              对流中元素的整数属性求和
  * inttotal=list.stream().collect(Collectors.summingInt(Employee::getSalary));
+ *
  * averagingInt               Double               计算流中元素Integer属性的平均值
  * doubleavg= list.stream().collect(Collectors.averagingInt(Employee::getSalary));
+ *
  * summarizingInt            IntSummaryStatistics   收集流中Integer属性的统计值。如：平均值
  * IntSummaryStatisticsiss= list.stream().collect(Collectors.summarizingInt(Employee::getSalary));
+ *
  * joining                   String                 连接流中每个字符串
  * String str= list.stream().map(Employee::getName).collect(Collectors.joining());
+ *
  * maxBy                     Optional<T>            根据比较器选择最大值
  * Optional<Emp>max= list.stream().collect(Collectors.maxBy(comparingInt(Employee::getSalary)));
+ *
  * minBy                     Optional<T>            根据比较器选择最小值
  * Optional<Emp> min = list.stream().collect(Collectors.minBy(comparingInt(Employee::getSalary)));
+ *
  * reducing                  归约产生的类型            从一个作为累加器的初始值开始，利用BinaryOperator与流中元素逐个结合，从而归约成单个值
  * inttotal=list.stream().collect(Collectors.reducing(0, Employee::getSalar, Integer::sum));
+ *
  * collectingAndThen         转换函数返回的类型         包裹另一个收集器，对其结果转换函数
  * inthow= list.stream().collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
+ *
  * groupingBy                Map<K, List<T>>         根据某属性值对流分组，属性为K，结果为V
  * Map<Emp.Status, List<Emp>> map= list.stream().collect(Collectors.groupingBy(Employee::getStatus));
+ *
  * partitioningBy            Map<Boolean, List<T>>   根据true或false进行分区
  * Map<Boolean,List<Emp>>vd= list.stream().collect(Collectors.partitioningBy(Employee::getManage));
  */
@@ -176,5 +191,38 @@ public class StreamCollect {
         List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");
         String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
         System.out.println(G7Countries);
+    }
+
+    // summaryStatistics  IntSummaryStatistics ：一个状态对象，用于收集计数、最小值、最大值、总和和平均值等统计信息。
+    @Test
+    public void test10() {
+        //获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt(x -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+
+        IntStream stream = IntStream.of(4, 5, 6, 7);
+        IntSummaryStatistics summary_data = stream.summaryStatistics();
+        System.out.println(summary_data);
+    }
+
+    // mapToInt  返回一个IntStream，其中包括将给定函数应用于此流的元素的结果。
+    @Test
+    public void test11() {
+        //Stream mapToInt(ToIntFunction mapper)返回一个IntStream，其中包括将给定函数应用于此流的元素的结果。
+        // 既然有mapToInt，那么必定有mapToDouble,mapToLong等操作，不过学习了这个其它的就一目了然了。
+
+        //示例1:mapToInt()具有将流元素除以3的功能。
+        List<String> list = Arrays.asList("3", "6", "8", "14", "15");
+        list.stream().mapToInt(num -> Integer.parseInt(num))
+                .filter(num -> num % 3 == 0)
+                .forEach(System.out::println);
+
+        //示例2:mapToInt()在执行具有其长度的映射字符串的操作后返回IntStream。
+        List<String> list1 = Arrays.asList("Geeks", "for", "gfg","GeeksforGeeks", "GeeksQuiz");
+        list1.stream().mapToInt(str->str.length()).forEach(System.out::println);
     }
 }
